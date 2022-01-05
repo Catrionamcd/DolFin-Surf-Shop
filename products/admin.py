a5879848bc08832a724e3f4f688e2a893831d1dc
Catrionamcd <catrionamcd@gmail.com>
2022-01-05 10:24:05 +0000

    Customize admin.py for the products models & install pillow for images

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,91 @@
-from django.contrib import admin
+"""
+    Admin for the models in the Product App. This includes the
+    Category Model, the Brand Model, the Colour Model, the
+    Product Model and the Product Inventory Model.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Brand, Colour
+from .models import Product, ProductInventory
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+        Admin for the for the Category Model.
+    """
+    prepopulated_fields = {'category_name_slug': ('category_name',)}
+    fields = ('category_name', 'category_name_slug', 'sale_percent',
+              'giftcard_category')
+    list_display = (
+        'category_name',
+        'category_name_slug',
+        'sale_percent',
+        'giftcard_category'
+    )
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    """
+        Admin for the for the Brand Model.
+    """
+    prepopulated_fields = {'brand_name_slug': ('brand_name',)}
+    fields = ('brand_name', 'brand_name_slug')
+    list_display = (
+        'brand_name',
+        'brand_name_slug'
+    )
+
+
+@admin.register(Colour)
+class ColourAdmin(admin.ModelAdmin):
+    """
+        Admin for the for the Colour Model.
+    """
+    prepopulated_fields = {'colour_slug': ('colour',)}
+    fields = ('colour', 'colour_slug')
+    list_display = (
+        'colour',
+        'colour_slug'
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+        Admin for the for the Product Model.
+    """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+        'date_created'
+    )
+
+    ordering = ('sku',)
+
+
+@admin.register(ProductInventory)
+class ProductInventoryAdmin(admin.ModelAdmin):
+    """
+        Admin for the for the Product Inventory Model.
+    """
+    list_display = (
+        'product',
+        'colour',
+        'size',
+        'gender',
+        'length',
+        'quantity'
+    )
+
+
+# admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Brand, BrandAdmin)
+# admin.site.register(Colour, ColourAdmin)
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(ProductInventory, ProductInventoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,14 +1,24 @@
 """
     Models for the Product App. This includes the Category Model,
-    the Brand Model the Product Model and the Product Inventory Model
+    the Brand Model, the Colour Model, the Product Model and 
+    the Product Inventory Model
 """
 
 from django.db import models
 
 
 class Category(models.Model):
-
+    """
+        The Category Model will hold all categories of Products
+        sold on the DolFin Surf Shop, such as swimwear, wetsuits,
+        etc. It will hold an image for the category and a integer
+        for sales percentage discount for each category. It will also
+        hold a boolean field for giftcard category.
+    """
     class Meta:
+        """
+            Make Categories more readable on Admin Dashboard
+        """
         verbose_name_plural = 'Categories'
 
     category_name_slug = models.CharField(max_length=50, unique=True)
@@ -22,7 +32,9 @@ class Category(models.Model):
 
 
 class Brand(models.Model):
-
+    """
+        The Brand Model will hold all the different brand names
+    """
     brand_name_slug = models.SlugField(max_length=50, unique=True)
     brand_name = models.CharField(max_length=50, null=False, blank=False)
 
@@ -31,6 +43,10 @@ class Brand(models.Model):
 
 
 class Colour(models.Model):
+    """
+        The Colour Model will hold the different colours for different
+        products.
+    """
 
     colour_slug = models.SlugField(max_length=50, unique=True)
     colour = models.CharField(max_length=50, null=False, blank=False)
@@ -40,8 +56,11 @@ class Colour(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=False, blank=False, on_delete=models.SET_NULL)
-    brand = models.ForeignKey('Brand', null=False, blank=False, on_delete=models.SET_NULL)
+    """
+        The Product Model will hold the Product details
+    """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -49,7 +68,7 @@ class Product(models.Model):
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     has_gender = models.BooleanField(default=False, null=True, blank=True)
     has_colours = models.BooleanField(default=False, null=True, blank=True)
-    has_length = models.BooleanField(default=False, null= True, blank=True)
+    has_length = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -61,16 +80,72 @@ class Product(models.Model):
 
 
 class ProductInventory(models.Model):
-    product = models.ForeignKey('Product', null=True, blank=True, on_delete=models.SET_NULL)
-    colour = models.ForeignKey('Colour', null=True, blank=True, on_delete=models.SET_NULL)
-    size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
-    gender = models.CharField(max_length=2, null=True, blank=True) # M, F, JM, JF
-    length = models.CharField(max_length=2, null=True, blank=True) # L, M, S
-    quantity = models.IntegerField(null=False, blank=False, default=0)
+    """
+        The Product Inventory Model will hold the quantity of each product
+        available in stock.
+    """
 
+    class Meta:
+        """
+            Make Product Inventory more readable on Admin Dashboard
+        """
+        verbose_name_plural = 'Product Inventory'
+
+    product = models.ForeignKey('Product', null=True, blank=True,
+                                on_delete=models.SET_NULL)
+    colour = models.ForeignKey('Colour', null=True, blank=True,
+                               on_delete=models.SET_NULL)
+    XTRASMALL = 'XS'
+    SMALL = 'S'
+    MEDIUM = 'M'
+    LARGE = 'L'
+    XTRALARGE = 'XL'
+    SIZE_CHOICES = [
+        (XTRASMALL, 'XSmall'),
+        (SMALL, 'Small'),
+        (MEDIUM, 'Medium'),
+        (LARGE, 'Large'),
+        (XTRALARGE, 'XLarge'),
+    ]
+    size = models.CharField(
+        max_length=2,
+        null=True,
+        blank=True,
+        choices=SIZE_CHOICES,
+    )
+    MALE = 'M'
+    FEMALE = 'F'
+    JMALE = 'JM'
+    JFEMALE = 'JF'
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (JMALE, 'Junior Male'),
+        (JFEMALE, 'Junior Female'),
+    ]
+    gender = models.CharField(
+        max_length=2,
+        null=True,
+        blank=True,
+        choices=GENDER_CHOICES,
+    )
+    SHORT = 'S'
+    MID = 'M'
+    LONG = 'L'
+    LENGTH_CHOICES = [
+        (SHORT, 'Short'),
+        (MID, 'Mid-Length'),
+        (LONG, 'Long'),
+    ]
+    length = models.CharField(
+        max_length=2,
+        null=True,
+        blank=True,
+        choices=LENGTH_CHOICES,
+    )
+    quantity = models.IntegerField(null=False, blank=False, default=0)
 
     def __str__(self):
         return f'Product: {self.product}, Colour: {self.colour}, \
             Size: {self.size}, Gender: {self.gender}, Length: {self.length},\
             Quantity: {self.quantity}'
-
