04c4caa0456432894246777050b50ad77771ba77
Catrionamcd <catrionamcd@gmail.com>
2022-01-06 15:26:03 +0000

    Create the template, view & urls for site owner to add a product

--- products/models.py
+++ products/models.py
@@ -30,6 +30,9 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
 
+    def get_category_name(self):
+        return self.category_name
+
 
 class Brand(models.Model):
     """
