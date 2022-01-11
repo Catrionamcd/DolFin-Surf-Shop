fe3fcd2afbc12d12f1d17936d8ee00d74ca94085
Catrionamcd <catrionamcd@gmail.com>
2022-01-11 14:45:29 +0000

    Complete functionality of the webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,8 +1,3 @@
-"""
-    Admin for the models in the Checkout App. This includes the
-    Order Model, the Order line item Model. 
-"""
-
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
@@ -12,19 +7,20 @@ class OrderLineItemAdminInline(admin.TabularInline):
     readonly_fields = ('lineitem_total',)
 
 
-@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total', )
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
@@ -32,7 +28,4 @@ class OrderAdmin(admin.ModelAdmin):
 
     ordering = ('-date',)
 
-
-
-
-
+admin.site.register(Order, OrderAdmin)
