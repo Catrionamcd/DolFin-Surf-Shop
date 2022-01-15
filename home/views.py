2cb63162b0c6e72d64b2f5ccc89021207d40a512
Catrionamcd <catrionamcd@gmail.com>
2022-01-15 12:01:55 +0000

    Change Index.html to get categories from category model

--- home/views.py
+++ home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from products.models import Category
 
 # View to render the index page.
 
 
 def index(request):
     """A view to return the index page"""
-    return render(request, 'home/index.html')
+
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories
+    }
+
+    return render(request, 'home/index.html', context)
+
+
