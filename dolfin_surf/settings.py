132febf3053144a56668d6956f0f3a8f543d6abc
Catrionamcd <catrionamcd@gmail.com>
2022-01-16 16:00:59 +0000

    Change styling on error pages

--- dolfin_surf/settings.py
+++ dolfin_surf/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+DEBUG = False
+
 ALLOWED_HOSTS = ['dolfin-surf-shop.herokuapp.com', 'localhost']
 
 
