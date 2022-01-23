25f36e2f83ea08e5aebc653c69036315774af97b
Catrionamcd <catrionamcd@gmail.com>
2022-01-23 15:42:08 +0000

    Remove Colour section from main menu.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from products.models import Category, Brand, Colour, Product
+from products.models import Category, Brand, Product
 from django.db.models import Count
 
 # View to render the index page.
@@ -11,7 +11,6 @@ def index(request):
     categories = Category.objects.all().order_by('giftcard_category')
     categories_list = Category.objects.all().annotate(subcat_count=Count('subcategory'))
     brands = Brand.objects.all()
-    colours = Colour.objects.all()
     genders = Product.gender.field.choices
 
     """ On home page so update menu to turn off all category/sub-category checkboxes and turn on all others """
@@ -19,7 +18,6 @@ def index(request):
     cat_indeterminate = []
     sub_checked = []
     brand_checked = list(brands.values_list('id', flat=True))
-    colour_checked = list(colours.values_list('id', flat=True))   
     gender_checked_string = list([gender[0] for gender in genders])
     gender_checked = list(map(int, gender_checked_string))
     
@@ -28,7 +26,6 @@ def index(request):
     request.session['cat_indeterminate'] = cat_indeterminate
     request.session['sub_checked'] = sub_checked
     request.session['brand_checked'] = brand_checked
-    request.session['colour_checked'] = colour_checked
     request.session['gender_checked'] = gender_checked
 
     """ Check if any Sales in place """
@@ -43,13 +40,11 @@ def index(request):
         'sale_in_progress': sale_in_progress,
         'categories_list': categories_list,
         'brands': brands,
-        'colours': colours,
         'genders': genders,
         'cat_checked': cat_checked,
         'cat_indeterminate': cat_indeterminate,
         'sub_checked': sub_checked,
         'brand_checked': brand_checked,
-        'colour_checked': colour_checked,
         'gender_checked': gender_checked,
     }
 
--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q, Count
 from django.db.models.functions import Lower
-from .models import Product, Category, SubCategory, Brand, Colour
+from .models import Product, Category, SubCategory, Brand
 from .forms import ProductForm
 from checkout.models import Order, OrderLineItem
 from django.core.paginator import Paginator
@@ -28,9 +28,6 @@ def update_session(request):
     brand_checked = request.POST.getlist('brand_checked[]')
     brand_checked_num = list(map(int, brand_checked))
 
-    colour_checked = request.POST.getlist('colour_checked[]')
-    colour_checked_num = list(map(int, colour_checked))
-
     gender_checked = request.POST.getlist('gender_checked[]')
     gender_checked_num = list(map(int, gender_checked))
 
@@ -40,7 +37,6 @@ def update_session(request):
             request.session['cat_indeterminate'] = cat_indeterminate_num
             request.session['sub_checked'] = sub_checked_num
             request.session['brand_checked'] = brand_checked_num
-            request.session['colour_checked'] = colour_checked_num
             request.session['gender_checked'] = gender_checked_num
         except KeyError:
             return HttpResponse('Error')
@@ -56,14 +52,12 @@ def all_products(request):
     categories_list = Category.objects.all().annotate(subcat_count=Count('subcategory'))
     subcategories_list = SubCategory.objects.all()
     brands = Brand.objects.all()
-    colours = Colour.objects.all()
     genders = Product.gender.field.choices
 
     cat_checked = request.session.get('cat_checked', [])
     cat_indeterminate = request.session.get('cat_indeterminate', [])
     sub_checked = request.session.get('sub_checked', [])
     brand_checked = request.session.get('brand_checked', [])
-    colour_checked = request.session.get('colour_checked', [])
     gender_checked = request.session.get('gender_checked', [])
 
     category_filter = None
@@ -86,13 +80,12 @@ def all_products(request):
                 request.session['sub_checked'] = sub_checked
 
 
-    # if NO categories, sub-categories, brands, or colours retrieved from session
-    if cat_checked == [] and sub_checked == [] and brand_checked == [] and colour_checked == [] and gender_checked == []:
-        # then select all categories, sub-categories, brands and colours for view (except gift cards)
+    # if NO categories, sub-categories, brands, or gender retrieved from session
+    if cat_checked == [] and sub_checked == [] and brand_checked == [] and gender_checked == []:
+        # then select all categories, sub-categories, brands and genders for view (except gift cards)
         cat_checked = list(categories_list.exclude(giftcard_category=True).values_list('id', flat=True))
         sub_checked = list(subcategories_list.exclude(category__giftcard_category=True).values_list('id', flat=True))
         brand_checked = list(brands.values_list('id', flat=True))
-        colour_checked = list(colours.values_list('id', flat=True))
         gender_checked_string = list([gender[0] for gender in genders])
         gender_checked = list(map(int, gender_checked_string))
 
@@ -148,7 +141,6 @@ def all_products(request):
     context = {
         'categories_list': categories_list,
         'brands': brands,
-        'colours': colours,
         'genders': genders,
         'products': page_obj,
         'product_count': product_count,
@@ -159,7 +151,6 @@ def all_products(request):
         'cat_indeterminate': cat_indeterminate,
         'sub_checked': sub_checked,
         'brand_checked': brand_checked,
-        'colour_checked': colour_checked,
         'gender_checked': gender_checked,
         'is_paginated': True,
     }
